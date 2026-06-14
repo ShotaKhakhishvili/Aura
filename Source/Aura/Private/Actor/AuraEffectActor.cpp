a9374bbd8830df058dc0b1c48c5f4bbb83583d00
Shota Khakhishvili <94229156+ShotaKhakhishvili@users.noreply.github.com>
2026-06-14 16:37:51 +0200

    Section 4

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,57 @@
+
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+class IAbilitySystemInterface;
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	
+	Sphere = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	Sphere->SetupAttachment(Mesh);
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::OnEndOverlap);
+}
+
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// TODO: This is a very wrong way of accessing and changing attributes.
+	//		It will be replaced by ability effects in the future.
+	
+	if (IAbilitySystemInterface* AbilitySystemInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet* AuraAttributeSet = 
+			Cast<UAuraAttributeSet>(
+				AbilitySystemInterface
+				->GetAbilitySystemComponent()
+				->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		
+		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		MutableAuraAttributeSet->SetHealth(MutableAuraAttributeSet->GetHealth() + 25.0f);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+
+
+
